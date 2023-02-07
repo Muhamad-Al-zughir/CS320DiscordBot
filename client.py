433aac15c8afdbe51d1c7596ebf8ce8adde990b4
Muhamad-Al-zughir <m.alzughir@gmail.com>
2023-02-06 16:55:33 -0800

    Edited client.py, set message_content, bot can respond to hello command

--- client.py
+++ client.py
@@ -6,13 +6,24 @@ import os
 import discord
 from dotenv import load_dotenv
 
+# setting up the needed intents
+intents = discord.Intents.default()
+intents.message_content = True  # setting message_content to True in order to read messages
+
 load_dotenv() # loads all the content in the .env folder
 TOKEN = os.getenv('DISCORD_API')
 
 client = discord.Client(intents=discord.Intents.default())
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+
+# Code to respond to messages sent by users
+@client.event
+async def on_message(message):
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
 
 client.run(TOKEN)
