ce357286e1f40477d572d7f972ba735c2236bce3
Muhamad-Al-zughir <alzumuh000@outlook.com>
2023-02-09 21:12:52 -0800

    Added a slash command

--- client.py
+++ client.py
@@ -3,19 +3,25 @@
 
 import os
 import discord
+from discord import app_commands
 from dotenv import load_dotenv
 
 # setting up the needed intents
 intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
+# tree which will hold all of the client commands
+tree = app_commands.CommandTree(client)
+
+# the guildid of the server we want the bot to work in
+guildId = 1063209733764435998
 
 load_dotenv() # loads all the content in the .env folder
 TOKEN = os.getenv('DISCORD_API')
 
-client = discord.Client(intents=discord.Intents.all())
-
-
 @client.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=guildId))
     print(f'{client.user} has connected to Discord!')
 
 # Code to respond to messages sent by users
@@ -24,10 +30,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('!bye'):
+    if message.content.startswith('$bye'):
         await message.channel.send('Bye!')
-        
+    
+    # await client.process_commands(message)
+
+# Standard Command Format
+# sayhello command: Takes string input and bot will respond with hello to said string input
+@tree.command(name = 'sayhello', description = 'Bot will respond with hello to the input given', guild=discord.Object(id=guildId)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+@app_commands.describe(input="input")
+async def first_command(interaction: discord.Interaction, input: str):
+    await interaction.response.send_message(f'Hello {input}!')
+    
 client.run(TOKEN)
