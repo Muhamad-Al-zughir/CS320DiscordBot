e67bbc5cc8ca26b3a4d876639d06f81429a06f36
shawyan-t <shawyanrtg@gmail.com>
2023-02-11 16:29:52 -0800

    Added join feature for Discord Bot

--- music/muzique.py
+++ music/muzique.py
@@ -1,10 +1,16 @@
 # shbang
 # Started 1/18/2023
 
+# Imports
 import os
+import asyncio
 import discord
+from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
+import youtube_dl
+
+
 
 # setting up the needed intents
 intents = discord.Intents.all()
@@ -20,27 +26,24 @@ guildId = 1063209733764435998
 load_dotenv() # loads all the content in the .env folder
 TOKEN = os.getenv('DISCORD_API')
 
+# Member list of people in channel to be streamed to
+streaming_members = {}
+
+# YouTube options list set to best audio settings
+# Setting options found from youtube-dl documentation
+# https://github.com/ytdl-org/youtube-dl/blob/master/youtube_dl/YoutubeDL.py#L128-L278
+yt_params = {'format' :  'bestaudio/best'}
+yt_streamObj = youtube_dl.YoutubeDL(yt_params)
+
+# ffmpeg params set to disable video
+# Settings found from https://ffmpeg.org/ffmpeg.html
+ff_params = {'options' : '-vn'}
+
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=guildId))
     print(f'{client.user} has connected to Discord!')
 
-# Code to respond to messages sent by users
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-        
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-
-    if message.content.startswith('/chiefkeef'):
-        await message.channel.send("Fuckers in school telling me, always in the barber shop Chief Keef ain’t bout this, Chief Keef ain’t bout that My boy a BD on fucking Lamron and them He, he they say that nathan don’t be putting in no work SHUT THE FUCK UP! Y'all nathans ain’t know shit All ya motherfuckers talk about Chief Keef ain’t no hitta Chief Keef ain’t this Chief Keef a fake SHUT THE FUCK UP Y'all don’t live with that nathan Y'all know that nathan got caught with a ratchet Shootin' at the police and shit Nathan been on probation since fuckin, I don’t know when! Motherfuckers stop fuckin' playin' him like that Them nathans savages out there If I catch another motherfucker talking sweet about Chief Keef I’m fucking beating they ass! I’m not fucking playing no more You know those nathans role with Lil' Reese and them.")
-
-    if message.content.startswith('$bye'):
-        await message.channel.send('Bye!')
-    
-    # await client.process_commands(message)
 
 # Standard Slash Command Format
 # sayhello command: Takes string input and bot will respond with hello to said string input
@@ -49,6 +52,20 @@ async def on_message(message):
 async def say_hello(interaction: discord.Interaction, input: str):
     await interaction.response.send_message(f'Hello {input}!')
 
-    
+
+@tree.command(name = 'join', description = 'Bot will join your voice channel', guild=discord.Object(id=guildId))
+#@app_commands.describe(input="input")
+async def join(interaction: discord.Interaction, channel: discord.VoiceChannel):
+        """Joins a voice channel"""
+        try:
+            await interaction.response.send_message(f'Joining {channel}')
+            voice = interaction.guild
+            if voice.voice_client is not None:
+                return await voice.voice_client.move_to(channel)
+
+            await channel.connect()
+
+        except Exception as err:
+            print(err)
 
 client.run(TOKEN)
