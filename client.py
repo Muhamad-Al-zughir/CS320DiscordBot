e97ccb8fcee751c5c892bc805a79ccdf1cf2aca5
Muhamad-Al-zughir <alzumuh000@outlook.com>
2023-02-12 18:52:40 -0800

    Added Event class and addEvent function and command. Also added addprofile command

--- client.py
+++ client.py
@@ -54,17 +54,27 @@ async def basic_libgen(interaction, type: str, search: str): # Set the arguments
 async def spider_gif(interaction: discord.Interaction):
     await interaction.response.send_message('https://tenor.com/view/to-everyone-that-is-looking-for-this-spider-gif-gif-20691150')
 
-# listProfiles command: After the running of the command the bot will respond by posting a funny spider gif
+# listProfiles command: After the running of the command the bot will post all the profiles that have been created 10 profiles at a time. Users will be able to use buttons to view the other profiles if there are more than 10
 @tree.command(name = 'listprofiles', description = 'Bot will list out all the profiles created on this server')
 async def list_profiles_cmd(interaction: discord.Interaction):
-    path = "scheduler/" + str(interaction.guild.id) + ".json"   # grabbing the path of the json file for this server
-    check_file_size = os.stat(path).st_size # grabbing the file size, if check_file_size is 0 that means the file is empty and thus can be ignored
-    # checking file size
-    if(check_file_size == 0):
-        await interaction.response.send_message('No profiles have been created on this server!')
-    else:
-        # responding by printing out the profiles using discord embed features
-        await schedule.list_profiles(interaction)
+    # responding by printing out the profiles using discord embed features
+    await schedule.list_profiles(interaction)
+
+# addprofile command: Takes in profile name and profile notes after running the command the bot will create a profile with the given attributes. 
+# Name of the profile must not already be in use though. 
+@tree.command(name = 'addprofile', description = 'Bot will add a profile with the given name and notes')
+@app_commands.describe(name="Name of the profile to be created (NAME MUST NOT ALREADY BE IN USE)", notes="Any important notes regarding the profile")
+async def add_profile_cmd(interaction: discord.Interaction, name: str, notes: str):
+    await schedule.add_profile(interaction, name, notes)
+
+# addevent command: 
+@tree.command(name = 'addevent', description = 'Bot will add a profile with the given name and notes')
+@app_commands.describe(profile_name="Name of the profile for which this event should be added to", event_name="Name of the event to be added",
+                        event_notes="Notes regarding the event", start_hour="The hour the event starts (must be integer between 0 and 23 inclusive)",
+                        start_min="minute the event starts", end_hour="The hour the event ends at (must be integer between 0 and 23 inclusive)",
+                        end_min="The minute which the event ends at")
+async def add_event_cmd(interaction: discord.Interaction, profile_name: str, event_name: str, event_notes: str, start_hour: int, start_min: int, end_hour: int, end_min: int):
+    await schedule.add_event(interaction, profile_name, event_name, event_notes, start_hour, start_min, end_hour, end_min)
 
 # client event to take place whenever the client joins a server.
 # it will create a new json file in the scheduler directory to store the data associated with this newly joined guild
--- scheduler/schedule.py
+++ scheduler/schedule.py
@@ -1,25 +1,50 @@
 import json
 import discord
-from discord.ui import Button, View
+import os
+import basic.methods as bm
+from discord.ui import Button, View    
 from datetime import time
 
+# Event class will store all of the information regarding a particular event. 
+class Event:
+    start_time = time()
+    end_time = time()
+    def __init__(self, name, notes, starthour, endhour, startmin, endmin):
+        self.name = name
+        self.notes = notes
+        self.start_time.hour = starthour
+        self.start_time.minute = startmin
+        self.start_time.hour = endhour
+        self.start_time.min = endmin
 
+# List profiles function. 
 async def list_profiles(interaction: discord.Interaction):
-    embed=discord.Embed(title="Profiles", description="All schedules in the server ")
+    path = "scheduler/" + str(interaction.guild.id) + ".json"   # grabbing the path of the json file for this server
+    check_file_size = os.stat(path).st_size # grabbing the file size, if check_file_size is 0 that means the file is empty and thus can be ignored
+    # checking file size
+    if(check_file_size == 0):
+        await interaction.response.send_message('No profiles have been created on this server!')
+        return
+    
+    embed=discord.Embed(title="Profiles", description="All schedules in the server ", color=0x8208d4)
     embed.add_field(name="Page 1/20", value="", inline=False)
     embed.set_footer(text="Joe\nMuhamad\nJudah\nShawyan\nAlex")
 
     # function is called whenever the left arrow button is clicked
     async def left_arrow_callback(interaction):
-        await interaction.response.send_message("Left arrow has been clicked!")
+        left_arrow.disabled = True
+        await interaction.response.edit_message(view=view)
+        await bm.follow_up(interaction, "Left arrow has been clicked")
     async def right_arrow_callback(interaction):
-        await interaction.response.send_message("Right arrow has been clicked!")
+        right_arrow.disabled = True
+        await interaction.response.edit_message(view=view)
+        await bm.follow_up(interaction, "Right arrow has been clicked!")
     
     # creating the buttons
-    left_arrow = Button(label="🡠")
+    left_arrow = Button(label="🡠", style=discord.ButtonStyle.primary)
     left_arrow.callback = left_arrow_callback
 
-    right_arrow = Button(label="🡢")
+    right_arrow = Button(label="🡢", style=discord.ButtonStyle.primary)
     right_arrow.callback = right_arrow_callback
 
     # Creating the view to display the buttons
@@ -27,3 +52,29 @@ async def list_profiles(interaction: discord.Interaction):
     view.add_item(left_arrow)
     view.add_item(right_arrow)
     await interaction.response.send_message(embed=embed, view=view)
+
+# Takes in profile name, profile notes, and the Interaction object. 
+# The function returns either 0, 1, or 2. A 0 indicates a failure to create the profile for any reason, a 1 represents successful 
+# creation of the profile, and a 2 represents that a profile with the same name already exists
+async def add_profile(interaction: discord.Interaction, name: str, notes: str):
+    profile_exists(interaction, name)
+    profile = {"name":name, "notes":notes, "events":[]}
+    print(profile)
+    await bm.send_msg(interaction, "Profile Successfully Created")
+
+async def add_event(interaction: discord.Interaction, profile_name: str, event_name: str, event_notes: str, start_hour: int, start_min: int, end_hour: int, end_min: int):
+    return
+
+# Takes in the name of the profile and the interaction object, searches through JSON file and checks of a profile of the given name already exists
+# returns a 0 if the profile doesn't exist, a 1 if it does
+def profile_exists(interaction: discord.Interaction, name: str):
+    return 0
+
+# Takes in 3 integers as input
+# makes sure that the number given as a parameter is less than or equal to the upper bound and greater than or equal to the lower bound
+# returns 0 if the number is not within bounds, returns 1 if the number is within the bounds
+def within_bounds(number: int, lowerBound: int, upperBound: int):
+    if(number >= lowerBound and number <= upperBound):
+        return 1
+    else:
+        return 0
